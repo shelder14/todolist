e0fde0fd6c95dee16b81fd49a19153f6332e5a11
shelder14 <shelder144@yandex.ru>
2020-01-27 16:45:01 +0300

    first

--- script.js
+++ script.js
@@ -6,15 +6,43 @@ $(document).ready(function() {
         	console.log(response);
             $('.table').append(
                 `<tbody>${response.results.map(function(n){
-			    return `<tr>
-			      <td>${n.id}</td>
-			      <td>${n.name}</td>
+			    return `<tr class="tr" data-tr="${n.id}">
+                    <td class="id">${n.id}</td>
+                    <td class="name">${n.name}</td>
+                    <td class="autor">${n.author}</td>
+                    <td><span class="del" data-del="${n.id}">x</span></td> 
+                    <td class="edit-block"><span class="edit" data-edit="${n.id}">+</span></td>                     
 			    </tr>`;
-			}3).join(',')}
+			}).join(',')}
 			  </tbody>`
 			);
 			
         }
 
+    });
+    $('body').on("click", ".del", function(){
+        let delid = $(this).attr('data-del');
+        $(`.tr[data-tr=${delid}]`).hide();
+    });
+    $('body').on("click", ".edit", function(){
+        let editid = $(this).attr('data-edit');
+        let nametext = $(`.tr[data-tr=${editid}] .name`).text();
+        let idtext = $(`.tr[data-tr=${editid}] .id`).text();
+        let authortext = $(`.tr[data-tr=${editid}] .author`).text();
+        $(`.tr[data-tr=${editid}] .name`).html(`<input name='name' value="${nametext}">`);
+        $(`.tr[data-tr=${editid}] .id`).html(`<input name='id' value="${idtext}">`);
+        $(`.tr[data-tr=${editid}] .author`).html(`<input name='author' value="${authortext}">`);
+        $(`.tr[data-tr=${editid}] .edit-block`).html(`<span class="save" data-save="${editid}">✓</span>`);
+        
+    }); 
+    $('body').on("click", ".save", function(){
+        let editid = $(this).attr('data-save');
+        let nameinput= $(`.tr[data-tr=${editid}] input[name='name']`).val();
+        let idinput= $(`.tr[data-tr=${editid}] input[name='id']`).val();
+        let authorinput= $(`.tr[data-tr=${editid}] input[name='author']`).val();
+        $(`.tr[data-tr=${editid}] .name`).html(`${nameinput}`);
+        $(`.tr[data-tr=${editid}] .id`).html(`${idinput}`);
+        $(`.tr[data-tr=${editid}] .author`).html(`${authorinput}`);
+        $(`.tr[data-tr=${editid}] .edit-block`).html(`<span class="edit" data-edit="${editid}">+</span>`);
     });
 });
